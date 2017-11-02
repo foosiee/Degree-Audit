419033e22a490b5f52d06c1e299685ebe9e17c31
foosiee <33300547+foosiee@users.noreply.github.com>
2017-11-02 11:59:22 -0400

    Optimization
    Only using one for loop for getting user input of classes and getting credit hours.

--- ProjectIndex.java
+++ ProjectIndex.java
@@ -8,29 +8,6 @@ import java.util.Arrays;
 public class ProjectIndex {
     public static void main(String[] args) {
         
-        /* Create scanners, typically thought you didn't need two but with only 
-        one it was causing a weird bug when asking for string input but having
-        two separate scanners solved it */
-        Scanner numInput = new Scanner(System.in);
-        Scanner stringInput = new Scanner(System.in);
-        
-        
-        // Propmt user for number of classes they have taken;
-        System.out.print("Enter number of classes you have taken: ");
-        int classNum = numInput.nextInt();
-        
-        
-        // Creates empty array to hold users class names;
-        String[] userSet = new String[classNum];
-        
-        
-        // Loop to enter names of classes up to previous input: classNum;
-        for (int i = 0; i < classNum; i++){
-                System.out.print("Enter the name of class " + (i+1) + " : ");
-		userSet[i] = stringInput.nextLine();
-        }
-        
-        
         // Creates empty hashmap to store all classes and credit hours in major;
         HashMap<String, Integer> classMap = new HashMap<String, Integer>();  
         
@@ -65,39 +42,53 @@ public class ProjectIndex {
         classMap.put("MET-2100 Engr. Mechanics: Statics",3);
         classMap.put("PHYS-2010 Tech Physics I",5);
         classMap.put("PHYS-2020 Tech Physics II",5);
-        classMap.put("Override", 129);
+        classMap.put("Override",129);
         
-         
-        // Creates an array of all keys;
-        String[] classSet = classMap.keySet().toArray(new String[classMap.size()]);
+        /* Create scanners, typically thought you didn't need two but with only 
+        one it was causing a weird bug when asking for string input but having
+        two separate scanners solved it */
+        Scanner numInput = new Scanner(System.in);
+        Scanner stringInput = new Scanner(System.in);
         
         
+        // Propmt user for number of classes they have taken;
+        System.out.print("Enter number of classes you have taken: ");
+        int classNum = numInput.nextInt();
+        
+        
+        // Creates empty array to hold users class names;
+        String[] userSet = new String[classNum];
         // initalize creditSum and creditHour;
         int creditSum = 0;
         int creditHours = 0;
         
         
-        /* For loop to iterate over each index of userSet. But instead we do 
-        that iteration in the hashMap.get method which returns the value of
-        a key. We know the users inputed class will be in the hashmap if they
-        inputted the class correctly. So we can use each index as a key put
-        it into the method to unlock the value(credit hour) */
-        for (int i = 0; i < userSet.length; i++){
-            creditHours = classMap.get(userSet[i]);
-            // Add creditHours to creditSum;
-            creditSum += creditHours; 
-            /*Remove key from hashMap so we can print out later what class
-            they have yet to take*/
-            classMap.remove(userSet[i]);
+        // Loop to enter names of classes up to previous input: classNum;
+        for (int i = 0; i < classNum; i++){
+                System.out.print("Enter the name of class " + (i+1) + " : ");
+		userSet[i] = stringInput.nextLine();
+                
+            /* This part of loop is to iterate over each index of userSet. But instead we do 
+            that iteration in the hashMap.get method which returns the value of
+            a key. We know the users inputed class will be in the hashmap if they
+            inputted the class correctly. So we can use each index as a key put
+            it into the method to unlock the value(credit hour) */
+                
+                creditHours = classMap.get(userSet[i]);
+                // Add creditHours to creditSum;
+                creditSum += creditHours; 
+                /*Remove key from hashMap so we can print out later what class
+                they have yet to take*/
+                classMap.remove(userSet[i]);
             
             
-            // If creditHours is >= 128 break loop as they can graduate;
-            if (creditHours >= 128){
-                System.out.println("Congratulations! You can graduate!");
-                break;
+                // If creditHours is >= 128 break loop as they can graduate;
+                if (creditHours >= 128){
+                    System.out.println("Congratulations! You can graduate!");
+                    break;
             }
         }
-        
+          
         // White space;
         System.out.println(" ");
         
@@ -130,7 +121,7 @@ public class ProjectIndex {
                 System.out.print(newClassSet[i] + ", ");
                 counter++;
                 
-                if (counter % 5 == 0){
+                if (counter % 4 == 0){
                     System.out.println(" ");
                 }
             }
